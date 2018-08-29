83dc6b96b0fc71158c39fe3a369ea43688cc4cf9
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2018-08-29 15:02:55 +0200

    insert data into postgres

---
+++ raw_insert.js
@@ -0,0 +1,102 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'school',
+    password: '1234',
+    port: 5432
+});
+
+const data = require('./data.json')
+
+let client;
+
+(async () => {
+
+    client = await pool.connect()
+
+    let result = await client.query('SELECT * FROM "Department"')
+    console.log('no departments yet', result.rows)
+
+    await insertDepartment(client, data.science)
+    console.log('Inserted department with Id', data.science.id)
+
+    //#region instructors
+    
+    data.john.departmentId = data.science.id
+    await insertInstructor(client, data.john)
+    data.jane.departmentId = data.science.id
+    await insertInstructor(client, data.jane)
+    console.log('\x1b[33mInserted instructors %o\x1b[0m', [data.john, data.jane])
+    //#endregion
+
+    //#region students
+
+    await insertStudent(client, data.mark)
+    await insertStudent(client, data.oliver)
+    await insertStudent(client, data.benji)
+    await insertStudent(client, data.bruce)
+    console.log('\x1b[32mInserted students %o\x1b[0m', [data.mark, data.oliver, data.benji, data.bruce])
+
+    //#endregion
+
+    //#region courses
+    data.math.departmentId = data.science.id
+    data.math.instructorId = data.john.id
+    await insertCourse(client, data.math)
+    data.biology.departmentId = data.science.id
+    data.biology.instructorId = data.jane.id
+    await insertCourse(client, data.biology)
+    console.log('\x1b[36mInserted courses %o\x1b[0m', [data.math, data.biology])
+
+    //#endregion
+
+    //#region course enrollment
+    await enrollCourse(client, data.mark, data.math)
+    await enrollCourse(client, data.oliver, data.math)
+    await enrollCourse(client, data.benji, data.biology)
+    await enrollCourse(client, data.bruce, data.biology)
+    //#endregion
+
+    await client.release()
+    await pool.end()
+
+})()
+.catch(async (err) => {
+    try {
+        await client.release()
+        await pool.end()
+    }
+    catch (e){ }
+    console.error('ERROR', err)
+})
+
+const insertDepartment = async (client, department) => {
+    const values = Object.values(department)
+    let result = await client.query('INSERT INTO "Department"("Name", "Location") VALUES ($1, $2) RETURNING "Id"', values)
+    department.id = result.rows[0].Id
+}
+
+const insertInstructor = async (client, instructor) => {
+    const values = Object.values(instructor)
+    let result = await client.query('INSERT INTO "Instructor"("FirstName", "LastName", "ContactPhone", "Department_Id") VALUES ($1, $2, $3, $4) RETURNING "Id"', values)
+    instructor.id = result.rows[0].Id
+}
+
+const insertStudent = async (client, student) => {
+    const values = Object.values(student)
+    let result = await client.query('INSERT INTO "Student"("FirstName", "LastName") VALUES ($1, $2) RETURNING "Id"', values)
+    student.id = result.rows[0].Id
+}
+const insertCourse = async (client, course) => {
+    const values = Object.values(course)
+    let result = await client.query('INSERT INTO "Course"("Name", "Credits", "Department_Id", "Instructor_Id") VALUES ($1, $2, $3, $4) RETURNING "Id"', values)
+    course.id = result.rows[0].Id
+}
+
+const enrollCourse = async (client, student, course) =>{
+    const values = [student.id, course.id]
+    let result = await client.query('INSERT INTO "Student_Course" VALUES ($1, $2) RETURNING *', values)
+    return result.rows[0]
+}
