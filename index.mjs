2e324994dbea92cf13fed186112efa540ace1591
Daniel González García <daniel.gonzalez@danfoss.com>
2023-01-03 16:32:45 +0100

    modernize sqlite sample

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import sqlite from "sqlite3"
+import { open } from "sqlite"
+
+process
+    .on("uncaughtException", e => console.error("UNCAUGHT", e))
+    .on("unhandledRejection", e => console.error("UNHANDLED", e))
+
+const db = await open({ filename: ":memory:", driver: sqlite.Database })
+
+// DDL
+await db.run("CREATE TABLE tbl (k INTEGER PRIMARY KEY ASC, name TEXT)")
+
+// data insertion
+const firstInsert = await db.prepare("INSERT INTO tbl VALUES(:k, :name)", { ":k": 1, ":name": "some name" })
+await firstInsert.run()
+await firstInsert.finalize()
+
+const secondInsert = await db.prepare("INSERT INTO tbl VALUES(?, ?)")
+await secondInsert.run(2, "other name")
+await secondInsert.finalize()
+
+// query
+const rows = await db.all("SELECT * FROM tbl")
+rows.forEach(r => {
+    console.info(r.k, r.name)
+})
+
+await db.close()
